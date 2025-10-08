43dd842e88ff74bcbefda38a3964ac78e2ddad79
Fatih <hakimfatih1156@gmail.com>
2025-10-08 11:49:36 +0700

    fix: margin top for project section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -16,7 +16,7 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => {
           Featured Projects
         </h2>
         
-        <div className="grid md:grid-cols-3 gap-8">
+        <div className="grid mt-8 md:grid-cols-3 gap-8">
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
